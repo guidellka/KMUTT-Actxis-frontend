e18fa8ee31525e88f65e011704ae2953a5147d84
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2020-01-24 16:49:32 +0700

    [UPDATE] edit Api.js

--- src/scripts/Api.js
+++ src/scripts/Api.js
@@ -1,8 +1,12 @@
 import axios from "axios"
+import https from 'https'
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_ENDPOINT,
-    responseType: "json"
-});
+    responseType: "json",
+    httpsAgent: new https.Agent({  
+        rejectUnauthorized: false
+    })
+})
 
 export default axiosInstance
